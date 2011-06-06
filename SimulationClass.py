91490900cb46791e0d5b7862b3042d693f367538
jeffkaplan@caltech.edu <jeffkaplan@caltech.edu>
2011-06-05 21:55:11 -0700

    Sketched Simulation Class more

--- SimulationClass.py
+++ SimulationClass.py
@@ -4,7 +4,7 @@
 from numpy import *
 
 class DatSet:
-
+    something="nothing"
 
 class Simulation:
 
@@ -13,9 +13,34 @@ class Simulation:
     #second item: HydroResolution
     # GrResolution could be a set of Npoints (Ex.  Nr, Ntheta, Nphi
     #     for spherical shells)
+    
+    ##The following 4 must have the same length
     Levs=[]
+    DatSets=[]          #This is the filename prefix
+    DatSetsLabels={}
+    Resolutions=[]
+
+    Legends=[]
+
+    Directory='./'
+
+    Name='sim'
+
+    timeConversionFactor = 4.926e-6
+    densityConversionFactor= 6.173e17
     
-    DataSets=[]
+
+    
+    def __init__():
+        Levs=['Lev0,Lev1,Lev2']
+        DatSets=['DensestPoint']          #This is the filename prefix
+        DatSetsLabels={'DensestPoint':1}
+        Resolutions=[45,45+6*3,45+2*6*3]
+        
+        Legends=[]
+        
+        Directory='./data/'
+        
+        Name='sim'
 
 
-    
